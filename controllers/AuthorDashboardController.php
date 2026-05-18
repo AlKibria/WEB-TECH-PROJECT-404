dd48875fc358a8859aa80a88f91530ba724ee8c1
nayonuddin5777-web <nayonuddin5777@gmail.com>
2026-05-18 12:57:24 +0600

    Update AuthorDashboardController.php

--- controllers/AuthorDashboardController.php
+++ controllers/AuthorDashboardController.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../config/app.php';
 require_once __DIR__ . '/../models/ArticleModel.php';
 require_once __DIR__ . '/../models/UserModel.php';
 require_once __DIR__ . '/../models/CommentModel.php';
-
+  
 class AuthorDashboardController {
     private $articleModel;
     private $userModel;
